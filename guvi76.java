f6946f200ae1360db2d257434054c59db2f4ce47
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-09-05 10:11:49 +0530

    Create guvi76.java

---
+++ guvi76.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=123,n=0;
+
+for(int i=1;i<=a;i++){
+
+if(a%i==0)
+
+n++;}
+
+if(n>2)
+
+System.out.println("yes");
+
+else
+
+System.out.println("no");
+
+}
+}
